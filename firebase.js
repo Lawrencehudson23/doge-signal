a0bf1247df152afb51ddf90c708319d5a1b8576c
Lawrence Hudson <lawrence@Lawrences-Mac-mini.local>
2021-05-30 02:58:32 -0700

    first commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA5OI-8WDmUm9s8b53zKiEVAJYdDyIhEvU",
+  authDomain: "signal-clone-19c06.firebaseapp.com",
+  projectId: "signal-clone-19c06",
+  storageBucket: "signal-clone-19c06.appspot.com",
+  messagingSenderId: "70707383183",
+  appId: "1:70707383183:web:0531f97c6b591217c1d9fa"
+};
---
+++ screens/LoginScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {StyleSheet, Text, View} from 'react-native';
+
+const LoginScreen = () => {
+  return (
+      <View>
+        <Text>I am the login screen</Text>
+      </View>
+  );
+};
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({});
